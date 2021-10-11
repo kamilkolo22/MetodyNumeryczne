df35135c9f5ce5cfa9cde0cae5d91bc116364702
Kamil <kamil.kolodziejski44@gmail.com>
2021-10-11 21:16:36 +0200

    Permutation generating function

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import numpy as np
 
 
+def add_number(lista, n):
+    temp = []
+    for i in range(1, n+1):
+        if i not in lista:
+            lista_temp = lista.copy()
+            lista_temp.append(i)
+            temp.append(lista_temp)
+    return temp
+
+
 def generate_permutations(n):
     lista = [[x] for x in range(1, n+1)]
-    for i in range(1, n+1):
-        for l in lista:
-            l.append(i)
+
+    for t in range(n-1):
+        temp = []
+        for x in lista:
+            temp = temp + add_number(x, n)
+        lista = temp.copy()
+
     return lista
 
 
@@ -19,4 +33,4 @@ if __name__ == '__main__':
                  [4, 7, 9]])
     print(np.linalg.det(m))
 
-    print(generate_permutations(5))
+    print(generate_permutations(3))
